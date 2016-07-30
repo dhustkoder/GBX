7b3d5dbc84c87a2320d3b6702183960373f086da
dhust <rafaelmoura.dev@gmail.com>
2016-07-30 18:23:31 -0300

    main.cpp
    exit fix

---
+++ Source/main.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char** argv)
+{
+	if(argc < 2) {
+		fprintf(stderr, "usage: %s <rom>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	
+
+	return EXIT_SUCCESS;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
