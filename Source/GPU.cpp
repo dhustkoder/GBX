007f61bbbd66ddf0e27f9656961eaaa3392fa0f3
Rafael Moura <rafaelmoura.dev@gmail.com>
2016-09-01 05:32:23 -0300

    basic OAM drawing added

--- Source/GPU.cpp
+++ Source/GPU.cpp
@@ -22,7 +22,6 @@ static const uint32_t colors[4] = {
 
 static void hblank(GPU* const gpu, const uint8_t* const vram, uint32_t* const gfx);
 
-
 static void draw_bg(const uint8_t* data,
 	const uint8_t* map,
 	const bool unsig_tiles,
@@ -33,23 +32,33 @@ static void draw_bg(const uint8_t* data,
 	const uint8_t lymod,
 	uint32_t* const gfx_line);
 
+static void draw_oam(const GPU& gpu,
+	const uint8_t* const vram,
+	const uint8_t* const oam, 
+	uint32_t* const gfx);
+
+static void draw_sprite(const GPU& gpu,
+	const uint8_t* data,
+	const uint8_t xpos,
+	const uint8_t ypos,
+	const uint8_t flags,
+	uint32_t* const gfx);
 
 inline uint16_t get_row_unsig(const uint8_t* const data,
 	const uint8_t* const map,
-	const uint8_t map_id);
+	const uint8_t map_x);
 
 
 inline uint16_t get_row_sig(const uint8_t* const data,
 	const uint8_t* const map,
-	const uint8_t map_id);
-
+	const uint8_t map_x);
 
 inline void draw_row(const uint16_t row,
 	const uint8_t pixbeg,
 	const uint8_t pixend,
 	const uint8_t xpos,
-	const uint8_t* pallete,
-	uint32_t* gfx_line);
+	const uint8_t* const pallete,
+	uint32_t* const gfx_line);
 
 
 
@@ -99,6 +108,7 @@ void Gameboy::UpdateGPU(const uint8_t cycles)
 			hblank(&gpu, memory.vram, memory.gfx);
 
 			if (gpu.ly != 144) {
+				draw_oam(gpu, memory.vram, memory.oam, memory.gfx);
 				set_mode(GPU::Mode::OAM);
 			} else {
 				hwstate.RequestInt(INT_VBLANK);
@@ -219,39 +229,71 @@ static void draw_bg(const uint8_t* data,
 		draw_row(last_row, 0, scxmod, 160 - scxmod, pallete, gfx_line);
 	}
 
-	uint16_t rows[20];
-
 	if (unsig_tiles) {
-		for (uint8_t r = x; r < 20; ++r)
-			rows[r] = get_row_unsig(data, map, r + scxdiv);
+		for (; x < 20; ++x) {
+			const auto row = get_row_unsig(data, map, x + scxdiv);
+			const uint8_t xpos = (x * 8) - scxmod;
+			draw_row(row, 0, 8, xpos, pallete, gfx_line);
+		}
 	} else {
-		for (uint8_t r = x; r < 20; ++r)
-			rows[r] = get_row_sig(data, map, r + scxdiv);
-	}
-
-	for (; x < 20; ++x) {
-		const uint16_t row = rows[x];
-		const uint8_t xpos = (x * 8) - scxmod;
-
-		for (uint8_t pix = 0; pix < 8; ++pix) {
-			uint8_t col_num = 0;
-			if (row & (0x80 >> pix))
-				++col_num;
-			if (row & (0x8000 >> pix))
-				col_num += 2;
-
-			gfx_line[xpos + pix] = colors[pallete[col_num]];
+		for (; x < 20; ++x) {
+			const auto row = get_row_sig(data, map, x + scxdiv);
+			const uint8_t xpos = (x * 8) - scxmod;
+			draw_row(row, 0, 8, xpos, pallete, gfx_line);
 		}
 	}
 }
 
 
 
+
+static void draw_oam(const GPU& gpu,
+	const uint8_t* const vram, 
+	const uint8_t* const oam, 
+	uint32_t* const gfx)
+{
+	for (uint8_t x = 0; x < 40; ++x) {
+		const uint8_t ypos = oam[x * 4] - 16;
+		const uint8_t xpos = oam[x * 4 + 1] - 8;
+		if (ypos < 144 && xpos < 160) {
+			const uint8_t id = oam[x * 4 + 2];
+			const uint8_t flags = oam[x * 4 + 3];
+			const uint8_t* const data = vram + (id * 16);
+			draw_sprite(gpu, data, xpos, ypos, flags, gfx);
+		}
+	}
+}
+
+
+static void draw_sprite(const GPU& gpu,
+	const uint8_t* data,
+	const uint8_t xpos,
+	const uint8_t ypos,
+	const uint8_t flags,
+	uint32_t* const gfx)
+{
+	const auto obp = (flags & 0x10) ? gpu.obp1 : gpu.obp0;
+
+	const uint8_t pallete[4] = {
+		static_cast<uint8_t>(obp&0x03),
+		static_cast<uint8_t>((obp&0x0C) >> 2),
+		static_cast<uint8_t>((obp&0x30) >> 4),
+		static_cast<uint8_t>((obp&0xC0) >> 6),
+	};
+	
+	for (uint8_t y = 0; y < 8; ++y) {
+		const uint16_t row = (data[(y * 2) + 1] << 8) | data[y * 2];
+		uint32_t* const line = &gfx[(ypos + y) * 160];
+		draw_row(row, 0, 8, xpos, pallete, line);
+	}
+}
+
+
 inline void draw_row(const uint16_t row,
 	const uint8_t pixbeg,
-	const uint8_t pixend,
+	const uint8_t pixend, 
 	const uint8_t xpos,
-	const uint8_t* pallete,
+	const uint8_t* const pallete,
 	uint32_t* gfx_line)
 {
 	for (uint8_t pix = pixbeg; pix < pixend; ++pix) {
@@ -270,9 +312,9 @@ inline void draw_row(const uint16_t row,
 
 inline uint16_t get_row_unsig(const uint8_t* const data,
 	const uint8_t* const map,
-	const uint8_t map_id)
+	const uint8_t map_x)
 {
-	const uint8_t tile_id = map[map_id];
+	const uint8_t tile_id = map[map_x&31];
 	const uint16_t addr = tile_id * 16;
 	return (data[addr + 1] << 8) | data[addr];
 }
@@ -281,12 +323,22 @@ inline uint16_t get_row_unsig(const uint8_t* const data,
 
 inline uint16_t get_row_sig(const uint8_t* const data,
 	const uint8_t* const map,
-	const uint8_t map_id)
+	const uint8_t map_x)
 {
-	const int8_t tile_id = map[map_id];
+	const int8_t tile_id = map[map_x&31];
 	const int16_t addr = tile_id * 16;
 	return (*(data + addr + 1) << 8) | *(data + addr);
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
